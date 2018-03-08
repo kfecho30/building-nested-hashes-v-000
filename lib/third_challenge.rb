7f3c41c0b89345c60a4c24f07946bee518f2ae22
Kelsey Fecho <kfecho30@gmail.com>
2018-03-08 20:24:10 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,10 +10,12 @@ def third_challenge
         :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -28,5 +30,5 @@ def third_challenge
    }
   }
 
-  
+
 end
